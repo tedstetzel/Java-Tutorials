ad530d0168be4dd3de6229c4b9208ef5f973977b
sam-spade <ted.stetzel@gmail.com>
2015-06-16 10:10:07 -0700

    First three chapters
    -Basics
    -Variables
    -Keyboard input

---
+++ UsingVariables.java
@@ -0,0 +1,18 @@
+public class UsingVariables 
+{
+	public static void main(String[] args)
+	{
+	
+	int room;
+	double eVar;
+	String subjectName;
+	
+	room = 113;
+	eVar = 2.71828;
+	subjectName = "Computer Science";
+	
+	System.out.println("This is room " + room);
+	System.out.println("e is close to " + eVar);
+	System.out.println("I am learning a bit about " + subjectName);
+	}
+}
